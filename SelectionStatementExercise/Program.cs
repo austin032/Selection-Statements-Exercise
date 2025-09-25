1f9add89639045978e5aba373b155bee98304194
Austin Okyere <okyereaustin@gmail.com>
2025-09-25 14:58:40 -0600

    Completed StatementExercise1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 24;
+            
+            Console.WriteLine("Guess my favorite number:");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"The number you entered is too low!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"The number you entered is too high!");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed the number correctly!");
+            }
         }
     }
 }
